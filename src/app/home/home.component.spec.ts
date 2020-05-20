892629e23047e46e822f4a25ec257deae88c3122
Luka Toni <luka.toni93@gmail.com>
2020-05-20 19:29:25 +0200

    Created POST request for token
    Added html page for input username, password and authorization key for
    POST request.
    Prepared README.md for demo browser that allow CORS.
    
    No task associated.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,8 +1,5 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { Component, Input, DebugElement } from '@angular/core';
 
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,112 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
-import { LegendItem, ChartType } from '../lbd/lbd-chart/lbd-chart.component';
-import * as Chartist from 'chartist';
+import { Component, OnInit, Input } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
+
+@Injectable({
+    providedIn: 'root'
+})
+
 export class HomeComponent implements OnInit {
-    public emailChartType: ChartType;
-    public emailChartData: any;
-    public emailChartLegendItems: LegendItem[];
 
-    public hoursChartType: ChartType;
-    public hoursChartData: any;
-    public hoursChartOptions: any;
-    public hoursChartResponsive: any[];
-    public hoursChartLegendItems: LegendItem[];
+    private REST_API_SERVER = "http://rdweb.spica.com:5213/";
 
-    public activityChartType: ChartType;
-    public activityChartData: any;
-    public activityChartOptions: any;
-    public activityChartResponsive: any[];
-    public activityChartLegendItems: LegendItem[];
-    constructor() { }
+    private authKey: string = "";
+    private username: string = "";
+    private password: string = "";
 
-    ngOnInit() {
-        this.emailChartType = ChartType.Pie;
-        this.emailChartData = {
-            labels: ['62%', '32%', '6%'],
-            series: [62, 32, 6]
-        };
-        this.emailChartLegendItems = [
-            { title: 'Open', imageClass: 'fa fa-circle text-info' },
-            { title: 'Bounce', imageClass: 'fa fa-circle text-danger' },
-            { title: 'Unsubscribe', imageClass: 'fa fa-circle text-warning' }
-        ];
+    private tokenSecure: string = "";
 
-        this.hoursChartType = ChartType.Line;
-        this.hoursChartData = {
-            labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-            series: [
-                [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
-                [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
-                [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
-            ]
-        };
-        this.hoursChartOptions = {
-            low: 0,
-            high: 800,
-            showArea: true,
-            height: '245px',
-            axisX: {
-                showGrid: false,
-            },
-            lineSmooth: Chartist.Interpolation.simple({
-                divisor: 3
-            }),
-            showLine: false,
-            showPoint: false,
-        };
-        this.hoursChartResponsive = [
-            ['screen and (max-width: 640px)', {
-                axisX: {
-                    labelInterpolationFnc: function (value) {
-                        return value[0];
-                    }
-                }
-            }]
-        ];
-        this.hoursChartLegendItems = [
-            { title: 'Open', imageClass: 'fa fa-circle text-info' },
-            { title: 'Click', imageClass: 'fa fa-circle text-danger' },
-            { title: 'Click Second Time', imageClass: 'fa fa-circle text-warning' }
-        ];
+    private headers = {};
+    private body = JSON.stringify({});
 
-        this.activityChartType = ChartType.Bar;
-        this.activityChartData = {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [
-                [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-                [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-            ]
-        };
-        this.activityChartOptions = {
-            seriesBarDistance: 10,
-            axisX: {
-                showGrid: false
-            },
-            height: '245px'
-        };
-        this.activityChartResponsive = [
-            ['screen and (max-width: 640px)', {
-                seriesBarDistance: 5,
-                axisX: {
-                    labelInterpolationFnc: function (value) {
-                        return value[0];
-                    }
-                }
-            }]
-        ];
-        this.activityChartLegendItems = [
-            { title: 'Tesla Model S', imageClass: 'fa fa-circle text-info' },
-            { title: 'BMW 5 Series', imageClass: 'fa fa-circle text-danger' }
-        ];
+    ngOnInit() { }
 
-
+    dataChangedUsername(inputText) {
+        this.username = inputText;
     }
 
+    dataChangedPassword(inputText) {
+        this.password = inputText;
+    }
+
+    dataChanged(inputText) {
+        this.authKey = inputText;
+    }
+
+    constructor(private http: HttpClient) { }
+
+    // POST for TOKEN
+    public sendPostRequest() {
+        console.log("Send POST request");
+
+        this.body = JSON.stringify(
+            {
+                "Username": this.username,
+                "Password": this.password,
+                "Sid": ""
+            }
+        );
+
+        this.headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Authorization': 'SpicaToken ' + this.authKey
+        };
+
+        this.http.post(this.REST_API_SERVER + "timeapi/Session/GetSession",
+            this.body,
+            {
+                'headers': this.headers,
+                observe: 'response'
+            })
+            .subscribe(
+                data => {
+                    console.log("POST Request is successful ", data);
+                    this.tokenSecure = JSON.parse(data.body['Token']);
+                    console.log(this.tokenSecure);
+                },
+                error => {
+                    console.log("Error", error);
+                }
+            )
+    }
 }
